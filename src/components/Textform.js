bec27d64b526d9f153f068ce88953315f13593e3
Aryan Bhatnagar <aryanmusicbht@gmail.com>
2024-07-11 15:01:25 +0530

    Update Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,35 +1,32 @@
 import React, { useState } from "react";
 
- 
-export default function Textform(props) 
-
-
-{
+export default function Textform(props) {
   const [text, setText] = useState("");
+  
   const handleUpClick = () => {
-    console.log("Upper Case Was Clicked" + text);
     let newTxt = text.toUpperCase();
     setText(newTxt);
     props.showAlert("Converted To UpperCase","success")
   };
+  
   const handleOnChange = (event) => {
-    console.log("Upper Case Was Clicked");
     setText(event.target.value);
   };
-  
+
   const handleLcClick = () => {
-    console.log("Lower Case Was Clicked" + text);
     let newtxt = text.toLowerCase();
     setText(newtxt);
     props.showAlert("Converted To LoweCase","success")
   };
 
   const clear = () => {
-    console.log("Clear Was Clicked" + text);
     setText("");
   };
 
-
+  const copyText = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Text copied to clipboard", "success");
+  };
 
   return (
     <>
@@ -46,27 +43,29 @@ export default function Textform(props)
             value={text}
             onChange={handleOnChange}
           ></textarea>
-        </div >
-        <div  className ="d-flex p-2 flex-wrap gap-3">
-
-        <button className="btn btn-outline-danger" onClick={handleUpClick}>
-          CONVERT TO UPPER CASE
-        </button>
-        <button className="btn btn-outline-danger mx-2" onClick={handleLcClick}>
-          CONVERT TO LOWER CASE
-        </button>
-        <button className="btn btn-outline-danger mx-2" onClick={clear}>
-          CLEAR TEXT
-        </button>
+        </div>
+        <div className="d-flex p-2 flex-wrap gap-3">
+          <button className="btn btn-outline-danger" onClick={handleUpClick}>
+            CONVERT TO UPPER CASE
+          </button>
+          <button className="btn btn-outline-danger mx-2" onClick={handleLcClick}>
+            CONVERT TO LOWER CASE
+          </button>
+          <button className="btn btn-outline-danger mx-2" onClick={clear}>
+            CLEAR TEXT
+          </button>
+          <button className="btn btn-outline-danger mx-2" onClick={copyText}>
+            COPY TEXT
+          </button>
         </div>
       </div>
       <div className="m-2">
-        <h1>Your Text Summmay</h1>
+        <h1>Your Text Summary</h1>
         <p>
-          {text.split(" ").length - 1} words and {text.length} characters
+          {text.split(" ").filter(Boolean).length} words and {text.length} characters
         </p>
         <p>
-          {0.08 * text.split(" ").length} minutes you will take to read this{" "}
+          {0.08 * text.split(" ").filter(Boolean).length} minutes you will take to read this
         </p>
         <h1>Preview</h1>
         <p>{text}</p>
@@ -75,4 +74,5 @@ export default function Textform(props)
   );
 }
 
+ 
  
